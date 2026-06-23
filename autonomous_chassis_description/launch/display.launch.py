70f3f8b7dbb1f2874842bbab50ec8a99bf770933
Sivakumar Thirumurugan <sivakumarthirumurugan@gmail.com>
2026-06-23 21:23:57 +0530

    Refactor launch file to use xacro and streamline nodes

--- autonomous_chassis_description/launch/display.launch.py
+++ autonomous_chassis_description/launch/display.launch.py
@@ -2,49 +2,36 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import launch_ros.descriptions
 
 def generate_launch_description():
-    # Dynamically locate the install directory of your description package
-    pkg_description = get_package_share_directory('autonomous_chassis_description')
+    # Dynamically find where the system compiled the package
+    pkg_dir = get_package_share_directory('autonomous_chassis_description')
     
-    urdf_file = os.path.join(pkg_description, 'urdf', 'chassis.urdf.xacro')
-    rviz_config = os.path.join(pkg_description, 'rviz', 'urdf.rviz')
+    # Locate files smoothly within the share index
+    xacro_file = os.path.join(pkg_dir, 'urdf', 'chassis.urdf.xacro')
+    rviz_config = os.path.join(pkg_dir, 'rviz', 'chassis_view.rviz')
 
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            arguments=[urdf_file],
-            output='screen'
+            output='screen',
+            parameters=[{'robot_description': launch_ros.descriptions.ParameterValue(
+                os.popen(f'xacro {xacro_file}').read(), value_type=str
+            )}]
         ),
-       
-    with open(urdf_file, 'r') as infp:
-        robot_desc = infp.read()
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_desc}]
-    )
-
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui'
-    )
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen'
-    )
-
-    return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
+            output='screen'
+        )
     ])
